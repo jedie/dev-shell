11a507764b47158d304f0238b27be475baf7cbd8
JensDiemer <git@jensdiemer.de>
2021-03-19 12:58:34 +0100

    Create a generic CmdAppBaseTestCase

--- dev_shell/tests/fixtures.py
+++ dev_shell/tests/fixtures.py
@@ -1,23 +1,25 @@
 from unittest.case import TestCase
 
-
 from cmd2 import CommandResult
 from cmd2_ext_test import ExternalTestMixin
 
-from dev_shell.cmd2app import DevShellApp,  get_devshell_app_kwargs
+from dev_shell.cmd2app import DevShellApp, get_devshell_app_kwargs
 
 
-class DevShellAppTester(ExternalTestMixin, DevShellApp):
-    pass
+class CmdAppBaseTestCase(TestCase):
+    """
+    Base class for cmd2 app test cases
+    """
 
+    def get_app_instance(self):
+        """
+        :return: cmd2 app instance
+        """
+        raise NotImplemented
 
-class DevShellAppBaseTestCase(TestCase):
     def setUp(self):
         super().setUp()
-
-        # Init in the same way as in: bx_dev_setup.dev_setup.shell() !
-        self.app = DevShellAppTester(**get_devshell_app_kwargs())
-
+        self.app = self.get_app_instance()
         self.app.fixture_setup()
 
     def tearDown(self):
@@ -39,3 +41,19 @@ class DevShellAppBaseTestCase(TestCase):
             stderr = str(out.stderr)
 
         return stdout, stderr
+
+
+class DevShellAppTester(ExternalTestMixin, DevShellApp):
+    pass
+
+
+class DevShellAppBaseTestCase(CmdAppBaseTestCase):
+    """
+    Base class for dev-shell tests
+    """
+
+    def get_app_instance(self):
+        # Init the test app with the same kwargs as the real app
+        # see: dev_shell.cmd2app.devshell_cmdloop()
+        app = DevShellAppTester(**get_devshell_app_kwargs())
+        return app
