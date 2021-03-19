81808d56c636a6f4c0631f300e40925187e8639c
JensDiemer <git@jensdiemer.de>
2021-03-19 10:08:24 +0100

    Test on macos, too - TODO: Add windows support

--- dev_shell/command_sets/tests.py
+++ dev_shell/command_sets/tests.py
@@ -9,11 +9,16 @@ from dev_shell.utils.subprocess_utils import verbose_check_call
 @cmd2.with_default_category('Tests')
 class TestCommandSet(DevShellBaseCommandSet):
     def do_pytest(self, statement: cmd2.Statement):
-
+        """
+        Run dev-shell tests via pytest
+        """
         pytest = shutil.which('pytest')
-
-        # pytest_path = Path(sys.exec_prefix) / 'pytest'
-        # assert_is_file(pytest_path)
-
         popenargs = [pytest] + statement.arg_list
         verbose_check_call(*popenargs)
+
+    def do_list_venv_packages(self, statement: cmd2.Statement):
+        """
+        Just call "pip freeze" to list all installed venv packages
+        """
+        pip = shutil.which('pip')
+        verbose_check_call(pip, 'freeze')
